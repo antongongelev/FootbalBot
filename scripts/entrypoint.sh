7f894a09fdab0b25c1c9c5f842c120495f721b94
antongongelev <antongongelev@gmail.com>
2022-03-11 13:17:40 +0300

    change run scripts(11)

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,4 +7,4 @@ export JDBC_DATABASE_URL=jdbc:postgresql://${DATABASE_URL#*@}
 export JDBC_DATABASE_USERNAME=${WITHOUT_SCHEMA%%:*}
 export JDBC_DATABASE_PASSWORD=${WITHOUT_USERNAME%%@*}
 
-java -jar FootballBot-1.0-SNAPSHOT.jar -Xmx330m -Xss512k -Dspring.profiles.active=production
+java -Xmx330m -Xss512k -Dspring.profiles.active=production -jar FootballBot-1.0-SNAPSHOT.jar
